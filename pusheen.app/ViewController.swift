8bc07ef3d5adafb02c4c27188eba1fcb200b9725
kylesethgray <kylesgray@gmail.com>
2018-07-09 09:47:45 -0600

    changing config to match local machine

---
+++ pusheen.app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pusheen.app
+//
+//  Created by Kyle Gray on 7/9/18.
+//  Copyright © 2018 AVatsaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
